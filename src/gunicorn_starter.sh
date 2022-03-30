a78cb341914e321320881dc0515815efe82fde55
Adan Garcia <agquiroz@compartamos.com>
2022-03-29 20:54:33 -0600

    se agrega uvicorn & requirements

--- src/gunicorn_starter.sh
+++ src/gunicorn_starter.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #python manage.py db revision --rev-id fdfcb039ddfc
-python manage.py db migrate
-python manage.py db upgrade
-python manage.py run
+#python manage.py db migrate
+#python manage.py db upgrade
+#python manage.py run
+cd app
+python uvicorn main:app --reload
